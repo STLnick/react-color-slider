ac1dcc0b177ea5765145afa92186fdc7f24950c7
Nick Ray <nickrray@gmail.com>
2020-08-05 18:22:00 -0500

    :art: Bring in structure for fancy CSS slider
    Still needs previously made CSS classes to be fancy

--- src/components/ColorTool/Switch/index.js
+++ src/components/ColorTool/Switch/index.js
@@ -3,7 +3,15 @@ import PropTypes from 'prop-types'
 
 export const Switch = ({ handler }) => {
   return (
-    <input type="checkbox" onChange={handler} />
+    <div>
+      <span className="slider-label light-label">Lighter</span>
+      <label className="switch round">
+        <input className="switch-checkbox" onChange={handler} type="checkbox" />
+        <span className="slider round"></span>
+      </label>
+      <span className="slider-label dark-label not-selected">Darker</span>
+    </div>
+
   )
 }
 
